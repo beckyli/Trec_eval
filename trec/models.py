916079d6345a09f137b576685469d220398ecba5
James Macdonald <jameslmacdonald@hotmail.com>
2016-03-11 18:50:52 +0000

    finished models

--- trec/models.py
+++ trec/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class Researcher(models.Model):
 
-    user = models.CharField(max_length=128, unique=True)
+    username = models.CharField(max_length=128, unique=True)
     profile_pic = models.ImageField(name='avatar', width_field=200, height_field=200)
     website = models.CharField(max_length=1024)
     display_name = models.CharField(max_length=128)
     organisation = models.CharField(max_length=128)
 
     def __unicode__(self):
-        return self.user
+        return self.username
 
 class Track(models.Model):
 
@@ -30,7 +30,7 @@ class Task(models.Model):
     task_url = models.CharField(max_length=1024)
     description = models.CharField(max_length=1024)
     year = models.TimeField()
-    judgement_file = models.FileField()
+    judgementFile = models.FileField()
 
     def __unicode__(self):
         return self.title
@@ -45,3 +45,29 @@ class Run(models.Model):
     map = models.FloatField()
     p10 = models.FloatField()
     p20 = models.FloatField()
+
+    runTypes = (
+        ("a", "Automatic"),
+        ("m", "Manual"),
+    )
+    runType = models.CharField(max_length=1, choices=runTypes)
+
+    queryTypes = (
+        ("title", "Title"),
+        ("ti+des", "Title and description"),
+        ("dscrp", "Description"),
+        ("all", "All"),
+        ("other", "Other"),
+    )
+    queryType = models.CharField(max_length=6, choices=queryTypes)
+
+    feedbackTypes = (
+        ("none", "None"),
+        ("pseud", "Pseudo"),
+        ("rel", "Relevance"),
+        ("other", "Other")
+    )
+    feedbackType = models.CharField(max_length=5, choices=feedbackTypes)
+
+    def __unicode__(self):
+        return self.name
